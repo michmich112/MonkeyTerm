019d6b84d8a3a3b878254928d5593edbf07ac383
michmich112 <cantacuzene.michel@gmail.com>
2022-09-16 12:03:08 -0700

    Cleaning

--- src/tui/term.go
+++ src/tui/term.go
@@ -17,41 +17,39 @@ import (
 	"golang.org/x/term"
 )
 
-
-
 type Tty struct {
-	in *os.File
+	in  *os.File
 	out *os.File
-	fd int
+	fd  int
 }
 
 type Term struct {
-	L *sync.Mutex
+	L               *sync.Mutex
 	initialPosition [2]int
-	tty *Tty
-	isRaw bool
-	saved *term.State
-	terminal *term.Terminal	
-	obL *sync.Mutex
-	outBuf []byte
-	escapeCodes []int
-	started bool
-	reqBuf bool
-	escBuf []byte
+	tty             *Tty
+	isRaw           bool
+	saved           *term.State
+	terminal        *term.Terminal
+	obL             *sync.Mutex
+	outBuf          []byte
+	escapeCodes     []int
+	started         bool
+	reqBuf          bool
+	escBuf          []byte
 }
 
-func NewTerm(escapeCodes []int) (*Term, error){
+// Attempts to create a new managed Term
+func NewTerm(escapeCodes []int) (*Term, error) {
 	if len(escapeCodes) == 0 {
 		return new(Term), errors.New("At least one escape code is required.")
 	}
 
-	tin := os.Stdin
-	// tin := TtyIn()
+	tin := TtyIn()
 
 	tty := &Tty{
-		in: tin,
+		in:  tin,
 		out: os.Stderr,
-		fd: int(tin.Fd()),
+		fd:  int(tin.Fd()),
 	}
 
 	if !term.IsTerminal(tty.fd) {
@@ -65,17 +63,15 @@ func NewTerm(escapeCodes []int) (*Term, error){
 
 	nt := term.NewTerminal(tty.in, "")
 
-
 	// change from blocking to nonblock
 	syscall.SetNonblock(int(tin.Fd()), false)
 
 	t := &Term{
-		L: &sync.Mutex{},
-		// initialPosition: [2]int{x,y},
-		tty: tty,
-		isRaw: true,
-		saved: saved,
-		terminal: nt,
+		L:           &sync.Mutex{},
+		tty:         tty,
+		isRaw:       true,
+		saved:       saved,
+		terminal:    nt,
 		escapeCodes: escapeCodes,
 	}
 
@@ -84,18 +80,14 @@ func NewTerm(escapeCodes []int) (*Term, error){
 		return new(Term), err
 	}
 
-	t.initialPosition = [2]int{x,y}
+	t.initialPosition = [2]int{x, y}
 
-
-	return t,nil
+	return t, nil
 }
-
-
-
 
 // get the current x, y postion of the cursor for any tty (should be put in raw mode)
 // the lock should be held when doing this operation
-func (t *Term) getTerminalCursorPosition(tty *os.File) (x,y int, err error) {
+func (t *Term) getTerminalCursorPosition(tty *os.File) (x, y int, err error) {
 	tty.Write([]byte("\x1B[6n"))
 
 	var reader *bufio.Reader
@@ -110,22 +102,22 @@ func (t *Term) getTerminalCursorPosition(tty *os.File) (x,y int, err error) {
 	text, err := reader.ReadSlice('R')
 
 	if err != nil {
-		return 0,0, err
+		return 0, 0, err
 	}
-	fmt.Printf("Nst %+v", text)
 	if slices.Contains(text, byte(';')) {
 		re := regexp.MustCompile(`\d+;\d+`)
 		pos := strings.Split(re.FindString(string(text)), ";")
 		x, errx := strconv.Atoi(pos[0])
 		y, erry := strconv.Atoi(pos[1])
 		if errx != nil || erry != nil {
-			return 0,0, errors.New("Error parsing pos value to int")
+			return 0, 0, errors.New("Error parsing pos value to int")
 		}
-		return x,y, nil
+		return x, y, nil
 	}
 	return 0, 0, errors.New("Invalid Return from ANSI Sequence")
 }
 
+// Makes the term cursor invisible
 func (t *Term) MakeCursorInvisible() {
 	t.L.Lock()
 	t.outputBytes([]byte("\x1B[?25l"))
@@ -133,6 +125,7 @@ func (t *Term) MakeCursorInvisible() {
 	t.L.Unlock()
 }
 
+// Makes the term cursor visible
 func (t *Term) MakeCursorVisible() {
 	t.L.Lock()
 	t.outputBytes([]byte("\x1B[?25h"))
@@ -140,17 +133,19 @@ func (t *Term) MakeCursorVisible() {
 	t.L.Unlock()
 }
 
-func (t *Term) InitialX() (int) {
+// Get the initial X position of the canvas/cursor
+func (t *Term) InitialX() int {
 	return t.initialPosition[0]
 }
 
-func (t *Term) InitalY() (int) {
+// Get the initial Y position of the canvas/cursor
+func (t *Term) InitalY() int {
 	return t.initialPosition[1]
 }
 
 // Gets the available width of the terminal
 func (t *Term) GetWidth() (int, error) {
-	x,_,err :=  term.GetSize(t.tty.fd)
+	x, _, err := term.GetSize(t.tty.fd)
 	if err != nil {
 		return 0, err
 	}
@@ -162,10 +157,11 @@ func (t *Term) GetAvailableHeight() (int, error) {
 	if y, err := t.GetHeight(); err != nil {
 		return y, err
 	} else {
-		return y-t.InitalY(), nil
+		return y - t.InitalY(), nil
 	}
 }
 
+// Get the height of the term
 func (t *Term) GetHeight() (int, error) {
 	_, y, err := term.GetSize(t.tty.fd)
 	if err != nil {
@@ -175,18 +171,18 @@ func (t *Term) GetHeight() (int, error) {
 }
 
 // gets the current x,y position of the cursor
-func (t *Term) GetCursorPosition() (x,y int, err error){
+func (t *Term) GetCursorPosition() (x, y int, err error) {
 	t.L.Lock()
-	x,y, err = t.getTerminalCursorPosition(t.tty.in)
+	x, y, err = t.getTerminalCursorPosition(t.tty.in)
 	t.L.Unlock()
 	return
 }
 
 // gets the curren x,y position of the cursor relative to the initial starting postition (only y)
-func (t *Term) GetRelativeCursorPosition() (x,y int, err error) {
-	x, y, err =  t.GetCursorPosition()
+func (t *Term) GetRelativeCursorPosition() (x, y int, err error) {
+	x, y, err = t.GetCursorPosition()
 	if err == nil {
-		y = y-t.InitalY()
+		y = y - t.InitalY()
 	}
 	return
 }
@@ -195,7 +191,7 @@ func (t *Term) GetRelativeCursorPosition() (x,y int, err error) {
 // Lock must be held
 // !!unsafe
 func (t *Term) getCursorPosition() (x, y int, err error) {
-	x,y, err = t.getTerminalCursorPosition(t.tty.in)
+	x, y, err = t.getTerminalCursorPosition(t.tty.in)
 	return
 }
 
@@ -204,29 +200,43 @@ func (t *Term) PeekUserInput() ([]byte, error) {
 	defer t.L.Unlock()
 	reader := bufio.NewReader(t.tty.in)
 	if reader.Buffered() > 0 {
-		a, err:= reader.Peek(reader.Buffered())
+		a, err := reader.Peek(reader.Buffered())
 		if err != nil {
 			return []byte{}, err
 		}
 		return a, err
 	}
-  return []byte{}, nil
+	return []byte{}, nil
 }
 
 // Appends an ANSI Goto position code to the output buffer
-func (t *Term) bufGotoPosition(x,y int) {
-	t.outBuf = append(t.outBuf, []byte(fmt.Sprintf("\x1B[%d;%dH",x,y))...)
+func (t *Term) bufGotoPosition(x, y int) {
+	t.outBuf = append(t.outBuf, []byte(fmt.Sprintf("\x1B[%d;%dH", x, y))...)
 	//_, err := t.tty.in.Write([]byte(fmt.Sprintf("\x1B[%d;%dH",y,x)))
+}
+
+// Appends an ANSI Goto position code to the output buffer
+// For transactional use only
+// !! unsafe
+func (t *Term) TxGotoPosition(x, y int) {
+	t.bufGotoPosition(x, y)
 }
 
 // Goes to the defined position on the terminal (not relative to starting point)
 func (t *Term) GotoPosition(x, y int) {
 	// maybe we should check if the values are outside the available canvas
-	// t.L.Lock()
+	t.L.Lock()
 	t.bufGotoPosition(x, y)
 	t.writeOutBuf() // write out all from the buffer
-	// t.L.Unlock()
-} 
+	t.L.Unlock()
+}
+
+// Goes to the desired relative position on the terminal relative to the starting point
+// For transactional use only
+// !! unsafe
+func (t *Term) TxGotoRelativePosition(x, y int) {
+	t.TxGotoPosition(x+t.InitialX(), y+t.InitalY())
+}
 
 // Goes to the desired position on the terminal relative to the starting point
 func (t *Term) GotoRelativePosition(x, y int) {
@@ -265,11 +275,14 @@ func (t *Term) outputBytes(bytes []byte) {
 	t.outBuf = append(t.outBuf, bytes...)
 }
 
-func (t *Term) UnsafeOutputBytes(bytes []byte) {
+// Outputs bytes to the output buffer to be printed
+// For transactional use only
+// !! unsafe
+func (t *Term) TxOutputBytes(bytes []byte) {
 	t.outputBytes(bytes)
 }
 
-// adds bytes to output buffer to be printed
+// Adds bytes to output buffer to be printed
 func (t *Term) OutputBytes(bytes []byte) {
 	t.L.Lock()
 	t.outputBytes(bytes)
@@ -290,19 +303,10 @@ func (t *Term) WriteRune(r rune) {
 	t.L.Unlock()
 }
 
-
-// Writes out the current contents of the output buffer until empty
-func (t *Term) WriteOutBuf() (error) {
-	t.L.Lock()
-	err := t.writeOutBuf()
-	t.L.Unlock()
-	return err
-}
-
 // Writes out the current contents of the buffer
 // Lock must be held before calling this
 // !!unsafe
-func (t *Term) writeOutBuf() (error) {
+func (t *Term) writeOutBuf() error {
 	l := len(t.outBuf)
 	for l > 0 {
 		ol, err := t.terminal.Write(t.outBuf)
@@ -322,7 +326,22 @@ func (t *Term) writeOutBuf() (error) {
 	return nil
 }
 
-func (t *Term) GoToPageStart() (error) {
+// Writes out the current contents of the output buffer
+// For transactional use only
+// !! unsafe
+func (t *Term) TxWriteOutBuf() error {
+	return t.writeOutBuf()
+}
+
+// Writes out the current contents of the output buffer until empty
+func (t *Term) WriteOutBuf() error {
+	t.L.Lock()
+	err := t.writeOutBuf()
+	t.L.Unlock()
+	return err
+}
+
+func (t *Term) GoToPageStart() error {
 	t.L.Lock()
 	t.bufGotoPosition(t.initialPosition[0], t.initialPosition[1])
 	err := t.writeOutBuf()
@@ -330,7 +349,7 @@ func (t *Term) GoToPageStart() (error) {
 	return err
 }
 
-func (t *Term) ClearToTopOfPage() (error) {
+func (t *Term) ClearToTopOfPage() error {
 	t.L.Lock()
 	// write out buffer so that all pending contents are good to go
 	// could technically be removed since we're going to clear it eitherway
@@ -343,28 +362,28 @@ func (t *Term) ClearToTopOfPage() (error) {
 
 	// fill output buffer with the action codes
 	t.bufGotoPosition(t.initialPosition[0], t.initialPosition[1])
-	for i := 0;i<=curx-t.initialPosition[0];i++ {
+	for i := 0; i <= curx-t.initialPosition[0]; i++ {
 		t.bufEraseEntireLine()
 		t.bufCursorDown()
 	}
 	t.bufGotoPosition(curx, cury)
 
 	// write buffer
-	t.writeOutBuf()	
+	t.writeOutBuf()
 
 	t.L.Unlock()
 	return nil
 }
 
-// func (t *Term) WritePage([]byte) (error) {
-// 	t.L.Lock()
-//
-// 	// Todo Implement this
-//
-// 	t.L.Unlock()
-// 	return nil
-// }
+// Saves the current cursor position (DEC)
+func (t *Term) SaveCursorPosition() {
+	t.OutputBytes([]byte("\x1B 7"))
+}
 
+// Restores the cursor to the last saved position (DEC)
+func (t *Term) RestoreCursorPosition() {
+	t.OutputBytes([]byte("\x1B 8"))
+}
 
 // restores the terminal to its pervious state
 func (t *Term) Restore() {
@@ -372,10 +391,10 @@ func (t *Term) Restore() {
 		panic("Unable to restore a non raw term")
 	}
 	t.isRaw = false
-	term.Restore(t.tty.fd,t.saved)
+	term.Restore(t.tty.fd, t.saved)
 }
 
-func (t *Term) ClearCanvas(){
+func (t *Term) ClearCanvas() {
 
 }
 
@@ -386,43 +405,6 @@ func (t *Term) ClearExit() {
 func (t *Term) IsEscapeCode(input byte) bool {
 	return slices.Contains(t.escapeCodes, int(input))
 }
-
-// func (t *Term) Start(actionFn func (event *TermEvent)) {
-// 	wg := sync.WaitGroup{}
-//
-// 	go func () {
-// 		reader := bufio.NewReader(t.tty.in)
-// 		b := make([]byte,1)
-// 		event := new(TermEvent)
-// 		active := true
-// 		for active {
-// 			l, err := reader.Read(b)
-// 			t.WriteRune('a')
-// 			t.WriteOutBuf()
-// 			if err != nil {
-// 				active = false
-// 				defer fmt.Printf("Read Error: %+v\n",err)
-// 			}
-// 			if l > 0 {
-// 				if slices.Contains(t.escapeCodes, int(b[0])) {
-// 					active = false	
-//
-// 				} else {
-// 					event.UpdateFromRune(rune(b[0]))
-// 					actionFn(event)
-// 				}
-// 				if len(t.outBuf) > 0 {
-// 					t.WriteOutBuf()
-// 				}
-// 			}
-// 		}
-// 		t.ClearExit()
-// 		wg.Done()
-// 	}()
-//
-// 	wg.Add(1)
-// 	wg.Wait()
-// }
 
 var CSI_END_CHAR = []rune{
 	'R', // Return from cursor position request
@@ -439,35 +421,26 @@ func (t *Term) HandleEscCode(buffer []byte) bool {
 	return false // this not good
 }
 
-func (t *Term) Start(handler func (t *Term, input byte, err error)) {
+func (t *Term) Start(handler func(t *Term, input byte, err error)) {
 	wg := sync.WaitGroup{}
 
-	go func () {
+	go func() {
 		reader := bufio.NewReader(t.tty.in)
-		b := make([]byte,1)
+		b := make([]byte, 1)
 		t.started = true
 		// event := new(TermEvent)
 		active := true
 
-		escCodeBuffer := make([]byte,0)
+		escCodeBuffer := make([]byte, 0)
 		buffering := false
 		for active {
 			l, err := reader.Read(b)
 
-		  //t.WriteRune('a')
-			// t.WriteOutBuf()
 			if err != nil {
 				active = false
-				fmt.Printf("Read Error: %+v\n",err)
+				fmt.Printf("Read Error: %+v\n", err)
 				handler(t, 0, err) // handler with error
-				panic("test")
 			}
-
-   		//
-			// if (t.reqBuf) {
-			// 	t.bufIn = append(t.bufIn, b[0])
-			// }
-   		//
 
 			if l > 0 {
 				if !buffering {
@@ -477,15 +450,15 @@ func (t *Term) Start(handler func (t *Term, input byte, err error)) {
 						continue // no op
 					}
 					if slices.Contains(t.escapeCodes, int(b[0])) {
-						active = false	
-					} 	
+						active = false
+					}
 					handler(t, b[0], nil) // handler call
 
 					if len(t.outBuf) > 0 {
 						t.WriteOutBuf()
 					}
 				} else {
-					escCodeBuffer = append(escCodeBuffer,b[0])
+					escCodeBuffer = append(escCodeBuffer, b[0])
 					if t.HandleEscCode(escCodeBuffer) { // this is the end of the esc sequence
 						// Share Escape Code
 						t.escBuf = escCodeBuffer
@@ -500,27 +473,19 @@ func (t *Term) Start(handler func (t *Term, input byte, err error)) {
 	}()
 
 	wg.Add(1)
+
 	wg.Wait()
 }
 
-
-func (t *Term) SaveCursorPosition() {
-	t.OutputBytes([]byte("\x1B 7"))
-}
-
-func (t *Term) RestoreCursorPosition() {
-	t.OutputBytes([]byte("\x1B 8"))
-}
-
 type TermTx struct {
-	t *Term
+	t       *Term
 	actions []func(t *Term)
 }
 
-func NewTermTx(t *Term) *TermTx{
+func NewTermTx(t *Term) *TermTx {
 	return &TermTx{
 		actions: make([]func(t *Term), 0),
-		t: t,
+		t:       t,
 	}
 }
 
@@ -532,11 +497,9 @@ func (t *TermTx) AddAction(f func(t *Term)) *TermTx {
 
 func (t *TermTx) Execute() {
 	t.t.L.Lock()
-	for _, f := range(t.actions) {
+	for _, f := range t.actions {
 		f(t.t)
 	}
-	t.t.writeOutBuf() //write out all changes
+	t.t.TxWriteOutBuf() //write out all changes
 	t.t.L.Unlock()
 }
-
-
